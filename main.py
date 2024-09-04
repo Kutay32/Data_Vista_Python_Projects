71171a11ade91eb5ea2204a3750b8f078accd689
Deryaakscc <kesiciderya9@gmail.com>
2024-09-04 21:37:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,50 @@
 import random
-import csv
+print("SAYI TAHMİN OYUNUMUZA HOS GELDİNİZ")
 Lider_tablosu={}
 def play_game():
     Takma_ad = input("Takma ad girin:")
     if Takma_ad in Lider_tablosu:
-        score=Lider_tablosu[Takma_ad]
-        print("Hoşgeldin,{Takma_ad} Mevcut puan:{score}")
-    else:
-        Lider_tablosu[Takma_ad]=score 
-    print("Zorluk seviyesi seçin:")
-    print("1.kolay (1-10)")
-    print("2.orta (1-50)")
-    print("3.zor (1-100)")
-    Seçiminiz=int(input("Seçiminiz (1,2,3:)"))
-    if seçim == 1:
-        max_sayı = 1 
-        doğru_tahmin = 10 
-    elif seçim == 2:
-        max_sayı = 50 
-        dogru_tahmin = 25
-    elif seçim == 3:
-        max_sayı =100 
-        doğru_tahmin = 50 
-    else:
-        print("Hatalı seçim")
-    doğru_tahmin=random.randint(1,max_sayı)
-    for hak in range(5)
-        guess=int(input(f"{hak+1} Tahmininizi gir (1-{max_number}:"))
-        if tahmin == doğru_tahmin:
-            print(f"Tebrikler,doğru tahmin.{puan} puan kazandınız")
-            score += puan
-            Lider_tablosud[Takma_ad] = score
-            break 
-        elif tahmin < doğru_tahmin:
-            print("Daha yüksek bir sayı söyleyin")
-        else:
-            print("Daha düşük bir sayı söyleyin")
-        if tahmin != doğru_tahmin:
-            print(f"yanlış bildiniz,doğru sayı {correct_guess}")
-            
-            
-        
+        puan=Lider_tablosu[Takma_ad]
+
+play_game()
+
+print("zorluk seviyesi seciniz: ")
+print("kolay (1-10 arası 10p)")
+print("orta (1-50 arası 25p)")
+print("zor (1-100 arası 50p)")
+
+
+secim = int(input("secimler (1/2/3)"))
+if secim == 1:
+    en_buyuk_sayi = 10
+    puan = 10
+elif secim == 2:
+    en_buyuk_sayi =50
+    puan = 25
+elif secim == 3:
+    en_buyuk_sayi = 100
+    puan = 100
+else:
+    print("yanlıs secim yaptınız.")
+
+
+
+tahmin = random.randint(1,100)
+deneme_hakki = 5
+
+while deneme_hakki > 0:
+
+    tahmin = int(input(f"1 ile en büyük sayi arasında tahmin ediniz: "))
+if tahmin == secim:
+    print("dogru tahmin tebrikler!")
+    toplam_puan += puan
+
+elif tahmin < secim:
+    print("sayıyı yükseltiniz!")
+else:
+    print("daha düşük sayi giriniz")
+    deneme_hakki -= 1
+if tahmin != secim:
+    print(f"yanlıs tahmin")
+
+
